1cc0a1545372124a255913180fc9d3e7ce87af54
Todd Demone <todd.demone.dev@gmail.com>
2022-04-13 12:22:22 -0400

    add admin_categories#index route, new controller, new view, new partial

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,8 @@
+class Admin::CategoriesController < ApplicationController
+  
+  http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD'] 
+
+  def index
+    @categories = Category.all
+  end
+end
