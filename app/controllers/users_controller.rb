a7d2a373ac8315ae45d5d8b20fa7977eb26fbbe4
Todd Demone <todd.demone.dev@gmail.com>
2022-04-16 17:51:00 +0000

    (1) user model: add name and email validations; (2) user controller: create new User object in 'new' method; (3) remove unnecessary buttons from signup and login views; (4) changed some misleading css classes in a few view files

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
@@ -9,7 +10,7 @@ class UsersController < ApplicationController
       session[:user_id] = user.id
       redirect_to '/'
     else
-      redirect_to '/signup'
+      render :new
     end
 
   end
@@ -17,7 +18,12 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(
+      :name,
+      :email,
+      :password,
+      :password_confirmation
+    )
   end
 
 end
