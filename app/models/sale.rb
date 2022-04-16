f75a547f74441562ac624fa02b2b33d21fc88e58
Todd Demone <todd.demone.dev@gmail.com>
2022-04-16 21:53:16 +0000

    add banner to home page that lists active sales

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,5 +1,9 @@
 class Sale < ActiveRecord::Base
 
+  # AR Scope (aka Class Methods)
+  def self.active # the 'self.' part makes it a Class Method
+    where("sales.starts_on <= ? AND sales.ends_on >= ?", Date.current, Date.current)
+  end
   def finished?
     ends_on < Date.current
   end
